f42a8c33c563f6d8e89f03e6a162833725823019
ZAI78 <Izaiahmedellin7878@gmail.com>
2024-09-17 18:50:19 -0500

    My Approach section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Approach from "@/components/Approach";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentsProjects from "@/components/RecentsProjects";
@@ -13,6 +14,7 @@ export default function Home() {
         <Hero />
         <Grid />
         <RecentsProjects />
+        <Approach />
       </div>
     </main>
   );
